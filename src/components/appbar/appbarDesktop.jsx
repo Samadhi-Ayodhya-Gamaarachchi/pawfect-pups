97905a25b1535cb0f0bb06a2ce1ae80458cfc1f5
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-02-06 23:22:49 +0530

    add nav bra

---
+++ src/components/appbar/appbarDesktop.jsx
@@ -0,0 +1,39 @@
+import { ActionIconContainerDesktop, ActionsIconContainerMobile, AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
+
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search"
+import Actions from "./action";
+export default function AppbarDesktop({matches}){
+
+    
+    return (
+        <>
+        
+        <AppbarContainer>
+            <AppbarHeader>Pawfect Pups</AppbarHeader>
+            <MyList type="row">
+                <ListItemText primary="Home"/>
+                <ListItemText primary="Categories"/>
+                <ListItemText primary="About us"/>
+                <ListItemText primary="Contact us"/>
+                <ListItemButton>
+                        <ListItemIcon>
+                            <SearchIcon />
+                        </ListItemIcon>
+                    </ListItemButton>
+
+            </MyList>
+            <Actions matches={matches}/>
+
+
+
+            </AppbarContainer>
+
+
+        
+
+       
+        </>
+        
+    )
+}
